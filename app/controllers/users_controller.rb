c8ea3e76c6c26fc35b28f56f706bcc9c10496509
knotchy <tkei.no303@gmail.com>
2018-12-03 12:44:19 +0900

    Update users_controller.rb
    currentユーザーの検出なし

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def search
-    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%").limit(10)
+    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%").where.not(id: current_user.id)
     respond_to do |format|
       format.html
       format.json
