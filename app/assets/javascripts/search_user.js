ae01e13a65579138a9fcd5debb2e2f3f48972314
knotchy <tkei.no303@gmail.com>
2018-12-05 20:32:20 +0900

    Update search_user.js

--- app/assets/javascripts/search_user.js
+++ app/assets/javascripts/search_user.js
@@ -20,7 +20,7 @@ $(document).on('turbolinks:load', function() {
   function appendChatMember(user_id,user_name) {
     var html = '<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-' + user_id + '">'+
                   '<input name="group[user_ids][]" type="hidden" value="' + user_id + '">'+
-                  '<p class='chat-group-user__name'>' + user_name + '</p>'+
+                  '<p class="chat-group-user__name">' + user_name + '</p>'+
                   '<a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>'+
                 '</div>';
   chat_member.append(html);
