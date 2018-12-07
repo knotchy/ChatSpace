f3d4c9949a9ef13fcc8474072201c80321710f91
knotchy <tkei.no303@gmail.com>
2018-12-07 12:23:38 +0900

    Update search_user.js

--- app/assets/javascripts/search_user.js
+++ app/assets/javascripts/search_user.js
@@ -3,26 +3,26 @@ $(document).on('turbolinks:load', function() {
   var chat_member = $('#chat-group-users')
 
   function appendUser(user) {
-    var html = '<div class="chat-group-user clearfix">'+
-                  '<p class="chat-group-user__name">'+ user.name + '</p>'+
-                    '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="' + user.id + '" data-user-name="' + user.name + '">追加</a>'+
-                '</div>';
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ user.name }</p>
+                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+                </div>`
     user_list.append(html);
   }
 
   function appendNoUser(user) {
-    var html = '<div class="chat-group-user clearfix">'+
-                      '<p class="chat-group-user__name">' + user + '</p>'+
-                '</div>';
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ user }</p>
+                </div>`
     user_list.append(html);
   }
 
   function appendChatMember(user_id,user_name) {
-    var html = '<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-' + user_id + '">'+
-                  '<input name="group[user_ids][]" type="hidden" value="' + user_id + '">'+
-                  '<p class="chat-group-user__name">' + user_name + '</p>'+
-                  '<a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>'+
-                '</div>';
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ user_id }'>
+                  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+                  <p class='chat-group-user__name'>${ user_name }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
   chat_member.append(html);
 }
 
@@ -36,7 +36,7 @@ $(document).on('turbolinks:load', function() {
     })
     .done(function(users) {
       $("#user-search-result").empty();
-      if (input.length !== 0){
+      if (users.length !== 0 && input.length !== 0 ){
         users.forEach(function(user) {
           appendUser(user);
         });
@@ -59,8 +59,8 @@ $(document).on('turbolinks:load', function() {
   });
 
   $(document).on("click",".chat-group-user__btn--remove",function(e) {
-    var user_id = $(this).data('id')
-    var user_name = $(this).data('name')
+    var user_id = $(this).data('user-id')
+    var user_name = $(this).data('user-name')
     $(this).parent().remove();
   });
 });
